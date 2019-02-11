ba07d0f366029982b50bc0e27fcb43223b7c6519
Roman Slyvka <roman.slyvka@7digital.com>
2019-02-11 16:33:37 +0200

    Refactor win function

--- Helpful/Standart.cs
+++ Helpful/Standart.cs
@@ -24,34 +24,83 @@ namespace Helpful
             table[row - 1, column - 1] = i;
         }
 
-
-
         public static bool Win(string[,] table, string i)
         {
-            if
-                (
-            //gorizontal
-            (object.Equals(table[0, 0], table[0, 1]) && object.Equals(table[0, 0], table[0, 2]) && object.Equals(table[0, 0], i)) ||
-            (object.Equals(table[1, 0], table[0, 1]) && object.Equals(table[1, 0], table[0, 2]) && object.Equals(table[1, 0], i)) ||
-            (object.Equals(table[2, 0], table[0, 1]) && object.Equals(table[2, 0], table[0, 2]) && object.Equals(table[2, 0], i)) ||
-            //vertical
-            (object.Equals(table[0, 0], table[1, 0]) && object.Equals(table[0, 0], table[2, 0]) && object.Equals(table[0, 0], i)) || 
-            (object.Equals(table[0, 1], table[1, 1]) && object.Equals(table[0, 1], table[2, 1]) && object.Equals(table[0, 1], i)) || 
-            (object.Equals(table[0, 2], table[1, 2]) && object.Equals(table[0, 2], table[2, 2]) && object.Equals(table[0, 2], i)) || 
-            //krest
-            (object.Equals(table[0, 0], table[1, 1]) && object.Equals(table[0, 0], table[2, 2]) && object.Equals(table[0, 0], i)) || 
-            (object.Equals(table[0, 2], table[1, 1]) && object.Equals(table[0, 0], table[2, 0]) && object.Equals(table[0, 2], i))
-            )
+            return IsHorizontalWin(table, i) || IsVerticalWin(table, i) ||
+                IsStraightDiagonal(table, i) || IsOppositeDiagonal(table, i);
+        }
+
+        private static bool IsStraightDiagonal(string[,] table, string symb)
+        {
+            for (int i = 0; i < table.GetLength(0); i++)
             {
-                return true;
+                if (table[i, i] != symb)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsOppositeDiagonal(string[,] table, string symb)
+        {
+            for (int i = 0, j = table.GetLength(1) - 1; i < table.GetLength(0) && j >= 0; i++, j--)
+            {
+                if (table[i, j] != symb)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsVerticalWin(string[,] table, string symb)
+        {
+            return IsTheSameLineInArray(table, symb, false);
+        }
+
+        private static bool IsHorizontalWin(string[,] table, string symb)
+        {
+            return IsTheSameLineInArray(table, symb, true);
+        }
+
+        private static bool IsTheSameLineInArray(string[,] table, string symb, bool horizontal)
+        {
+            int firstDimension, secondDimension;
+            if (horizontal)
+            {
+                firstDimension = 0;
+                secondDimension = 1;
             }
             else
             {
-                return false;
+                firstDimension = 1;
+                secondDimension = 0;
             }
 
-        }
 
+            for (int i = 0; i < table.GetLength(firstDimension); i++)
+            {
+                bool theSame = true;
+
+                for (int j = 0; j < table.GetLength(secondDimension); j++)
+                {
+                    if (horizontal)
+                    {
+                        if (table[i, j] != symb)
+                            theSame = false;
+                    }
+                    else
+                    {
+                        if (table[j, i] != symb)
+                            theSame = false;
+                    }
+                }
+
+                if (theSame)
+                    return true;
+            }
+
+            return false;
+        }
 
         public static void WriteTable(string[,] table)
         {
@@ -59,7 +108,5 @@ namespace Helpful
             Console.WriteLine("|" + table[1, 0] + "|" + table[1, 1] + "|" + table[1, 2] + "|");
             Console.WriteLine("|" + table[2, 0] + "|" + table[2, 1] + "|" + table[2, 2] + "|");
         }
-
-
     }
 }
